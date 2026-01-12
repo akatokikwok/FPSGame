406ab36b13dbdf129dac9e2a9e88417cc62b1bc6
Akatoki <akatokikwok@outlook.com>
2026-01-13 00:21:40 +0800

    [Feat]--[01A_02]: 制作SimpleFPSFeatureKit插件,为后续移植做准备.

---
+++ Plugins/SimpleFPSFeatureKit/Source/SimpleFPSFeatureKit/Private/SimpleFPSFeatureKitBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleFPSFeatureKitBPLibrary.h"
+#include "SimpleFPSFeatureKit.h"
+
+USimpleFPSFeatureKitBPLibrary::USimpleFPSFeatureKitBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USimpleFPSFeatureKitBPLibrary::SimpleFPSFeatureKitSampleFunction(float Param)
+{
+	return -1;
+}
+
